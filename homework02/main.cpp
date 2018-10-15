5543b1f76d542901a7638d1eae85243ed38f6372
zhouzhouzhou <julaoshi0416@163.com>
2018-10-15 23:02:57 +0800

    第二次作业：建立新的qt文件

--- homework02/main.cpp
+++ homework02/main.cpp
@@ -98,7 +98,8 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    Q_UNUSED(argc);
+    Q_UNUSED(argv);
 
     qInstallMessageHandler(myMessageOutput);
     QString datafile = "data.txt";
